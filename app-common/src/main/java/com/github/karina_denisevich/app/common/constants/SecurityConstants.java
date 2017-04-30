ed53ac1e3e2a59fe0de0b11dc33eefcb9bb25217
Karina Denisevich <karina_41997@mail.ru>
2017-04-30 13:12:35 +0300

    feat: add common(custom exceptions and constants)

---
+++ app-common/src/main/java/com/github/karina_denisevich/app/common/constants/SecurityConstants.java
@@ -0,0 +1,11 @@
+package com.github.karina_denisevich.app.common.constants;
+
+
+public final class SecurityConstants {
+
+    public final static String AUTH_HEADER_NAME = "token";
+
+    private SecurityConstants() {
+
+    }
+}
